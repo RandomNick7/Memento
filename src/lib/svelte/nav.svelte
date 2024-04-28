ed7279a74e7d3eb15cc7ce97f59fe65aa138074f
RandomNick7 <dorin.otgon@gmail.com>
2024-04-29 00:48:36 +0300

    Added navbar for all pages

---
+++ src/lib/svelte/nav.svelte
@@ -0,0 +1,31 @@
+<style>
+    nav{
+        margin-bottom: 12px;
+        display: flex;
+        background-color: hsl(200, 100%, 90%);
+        color: hsl(200, 100%, 40%);
+        font-size: 24px;
+        font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', sans-serif;
+    }
+
+    a{
+        padding: 8px;
+        text-decoration: none;
+    }
+
+    a:hover{
+        background-color: hsl(200, 100%, 85%);
+    }
+
+    a:active{
+        color: hsl(200, 100%, 40%);
+        background-color: hsl(200, 100%, 80%);
+    }
+</style>
+
+<nav id="navbar">
+    <a href="/">MiniDoc</a>
+    <a href="/editor">Editor</a>
+    <a href="/notes">Notes</a>
+</nav>
+<slot/>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,7 @@
 <script>
     import '../app.css'
+    import nav from '$lib/svelte/nav.svelte';
 </script>
 
+<svelte:component this={nav}/>
 <slot/>
