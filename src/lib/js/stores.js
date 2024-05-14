75848ce6905e903195d8d126cabc5aa7c340c66c
RandomNick7 <dorin.otgon@gmail.com>
2024-05-15 02:17:54 +0300

    Small navbar update

--- src/lib/js/stores.js
+++ src/lib/js/stores.js
@@ -2,15 +2,4 @@ import { writable } from 'svelte/store';
 import { persisted } from 'svelte-persisted-store'
 
 export const targetID = writable(0);
-
 export const darkTheme = persisted("Memento Theme", false)
-/*
-const defaultTheme = false;
-const initialTheme = browser ? window.localStorage.getItem("Memento Theme") ?? defaultTheme : defaultTheme;
-
-export const darkTheme = writable(false);
-darkTheme.subscribe((value) => {
-    if(browser){
-        return window.localStorage.setItem("Memento Theme", value);
-    }
-})*/
--- src/lib/svelte/nav.svelte
+++ src/lib/svelte/nav.svelte
@@ -7,7 +7,12 @@
     <a href="/">Memento</a>
     <a href="/editor">Editor</a>
     <a href="/notes">Notes</a>
-    <button on:click={() => {darkTheme.set(!$darkTheme)}}>
-        <img alt="Theme" src={$darkTheme? "/img/sun.svg" : "/img/moon.svg"}>
-    </button>
+    <div class="nav-right-side">
+      <div>Sign Up</div>
+      <div>Log In</div>
+      <div style="display:none">Sign Out</div>
+      <button on:click={() => {darkTheme.set(!$darkTheme)}}>
+          <img alt="Theme" src={$darkTheme? "/img/sun.svg" : "/img/moon.svg"}>
+      </button>
+    </div>
 </nav>
