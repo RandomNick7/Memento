b8bab3347eb9df946ef77def2bc035127f31a9fd
RandomNick7 <dorin.otgon@gmail.com>
2024-05-16 22:47:55 +0300

    Added complete login functionality

--- src/lib/svelte/nav.svelte
+++ src/lib/svelte/nav.svelte
@@ -1,8 +1,43 @@
 <script>
   import "$lib/css/navbar.css"
   import { darkTheme } from '$lib/js/stores.js';
+  import jsCookie from "js-cookie";
+  import { onMount } from "svelte";
 
   $: panel_state = 0;
+
+  function cookieCheck(){
+    if(jsCookie.get("MementoJWT")){
+      document.getElementById("sign-up").style.display = "none";
+      document.getElementById("log-in").style.display = "none";
+      document.getElementById("sign-out").style.display = "inline-block";
+      document.getElementById("backup").style.display = "inline-block";
+    }else{
+      document.getElementById("sign-up").style.display = "inline-block";
+      document.getElementById("log-in").style.display = "inline-block";
+      document.getElementById("sign-out").style.display = "none";
+      document.getElementById("backup").style.display = "none";
+    }
+  }
+
+  function parseJwt(token){
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    return JSON.parse(jsonPayload);
+  }
+
+  function expireCookie(){
+    jsCookie.remove("MementoJWT", {sameSite: "lax"});
+    cookieCheck();
+  }
+
+  onMount(async () => {
+    cookieCheck();
+  })
 
   function signUpDisplay(){
     panel_state != 1? panel_state = 1 : panel_state = -1;
@@ -16,6 +51,7 @@
     let user_field = document.getElementById("username").value;
     let pass_field = document.getElementById("password").value;
     let url = "http://randomnickname.pythonanywhere.com/" + (panel_state == 1? "sign_up" : "login")
+    
     let res = await fetch(url, {
       method: "POST",
       headers:{
@@ -27,8 +63,21 @@
       })
     })
     let token = await res.json();
-    console.log(token["token"]);
+
+    if(token["token"]){
+      let expiration_time = Date.parse(parseJwt(token["token"])["exp"]);
+      jsCookie.set("MementoJWT", token["token"], {expires: expiration_time, sameSite: "lax"});
+
+      let delta_time = expiration_time - Date.parse((new Date()).toUTCString());
+      setTimeout(expireCookie, delta_time);
+      cookieCheck();
+    }
     panel_state *= -1;
+  }
+
+  function signOut(){
+    jsCookie.remove("MementoJWT", {sameSite: "lax"});
+    cookieCheck();
   }
 </script>
 
@@ -37,10 +86,11 @@
       <a href="/">Memento</a>
       <a href="/editor">Editor</a>
       <a href="/notes">Notes</a>
+      <a href="/backup" id="backup">Backup</a>
       <div class="nav-right-side">
-        <div role="button" tabindex="0" on:keydown={()=>{}} on:click={signUpDisplay}>Sign Up</div>
-        <div role="button" tabindex="0" on:keydown={()=>{}} on:click={logInDisplay}>Log In</div>
-        <div style="display:none">Sign Out</div>
+        <div id="sign-up" role="button" tabindex="0" on:keydown={()=>{}} on:click={signUpDisplay}>Sign Up</div>
+        <div id="log-in" role="button" tabindex="0" on:keydown={()=>{}} on:click={logInDisplay}>Log In</div>
+        <div id="sign-out" role="button" tabindex="0" on:keydown={()=>{}} on:click={signOut} style="display:none">Sign Out</div>
         <button id="theme-swap" on:click={() => {darkTheme.set(!$darkTheme)}}>
             <img alt="Theme" src={$darkTheme? "/img/sun.svg" : "/img/moon.svg"}>
         </button>
---
+++ src/routes/backup/+page.svelte
@@ -0,0 +1,86 @@
+<script>
+  import "$lib/css/quill.snow.css";
+  import "$lib/css/notes.css";
+  import main from "$lib/svelte/main.svelte";
+  import { db } from "$lib/js/db";
+  import { liveQuery } from "dexie";
+  import { darkTheme } from "$lib/js/stores.js";
+  import jsCookie from "js-cookie";
+
+  let notes = liveQuery(
+    () => db.notes.toArray()
+  );
+
+  function deleteNote(){
+    async function deleteEntry(id){
+      db.notes.where('id').equals(id).delete();
+    }
+
+    let target = this.parentElement.parentElement;
+    deleteEntry(parseInt(target.dataset.id));
+  }
+</script>
+
+<style>
+  /* Cherry picked from quill.snow.css */
+  .note :global(.ql-font-serif){
+    font-family: Georgia, Times New Roman, serif;
+  }
+
+  .note :global(.ql-font-monospace){
+    font-family: Monaco, Courier New, monospace;
+  }
+
+  .note :global(.ql-size-small){
+    font-size:.75em;
+  }
+  
+  .note :global(.ql-size-large){
+      font-size:1.5em;
+  }
+
+  .note :global(.ql-size-huge){
+      font-size:2.5em;
+  }
+
+  .note :global(blockquote){
+    padding-left: 16px;
+    border-left: 4px solid #ccc;
+    margin-bottom: 5px;
+    margin-top: 5px;
+  }
+
+  .note :global(pre[data-language]){
+    padding: 5px 10px;
+    border-radius: 3px;
+    margin-bottom: 5px;
+    margin-top: 5px;
+    background-color:#23241f;
+    color:#f8f8f2;
+    font-family: monospace;
+  }
+</style>
+
+<svelte:component this={main}>
+  <div class={$darkTheme? "notes-wrapper dark-mode": "notes-wrapper"}>
+    {#if $notes}
+      {#if $notes.length > 0}
+        {#each $notes as note}
+          <div class="note" data-id={note.id} style="order:{note.pinned? -1 : note.id}">
+            <div class="controls">
+              <button on:click={deleteNote}>
+                <img alt="Delete" src="/img/trash_bin.svg">
+              </button>
+              <button>
+                
+              </button>
+            </div>
+            {@html note.html}
+          </div>
+        {/each}
+      {:else}
+        <div id="no-notes-text">Nothing saved yet...</div>
+      {/if}
+    {/if}
+  </div>
+</svelte:component>
